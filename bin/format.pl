6d49a11815f79002544b35531e27ded1fe539817
Francisco Souza <f@souza.cc>
2012-05-11 10:29:22 -0300

    format: added perl script for code formatting
    This script receives the c code from stdin, formats it using indent(1)
    and then use two regular expressions to remove leading spaces and other
    to put blank lines between procedures.

---
+++ bin/format.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl -w
+
+my @lines = <STDIN>;
+my $source = join('', @lines);
+
+my $filename = '/tmp/formatctmpfile21311.c';
+open TMP, '>', $filename;
+print TMP $source;
+close(TMP);
+
+system("indent", $filename);
+open TMP, $filename;
+@lines = <TMP>;
+close(TMP);
+
+$source = join('', @lines);
+$source =~ s/(\s){2,}$//m;
+$source =~ s/(\n}\n)([a-z])/$1\n$2/g;
+print $source;
