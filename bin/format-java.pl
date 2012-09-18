22bac2a26cca150047e9f8932c2ec864c6dceb5f
Francisco Souza <f@souza.cc>
2012-09-18 01:27:11 -0300

    all: added license information

--- bin/format-java.pl
+++ bin/format-java.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/env perl -w
 
+# Copyright 2012 Francisco Souza. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 my @lines = <STDIN>;
 my $source = join('', @lines);
 
